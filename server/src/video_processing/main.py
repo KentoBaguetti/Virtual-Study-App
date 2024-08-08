a38d6336d14b4d22bd184172ff1d1c60e802cec9
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-08-08 01:19:08 -0400

    cv2

--- server/src/video_processing/main.py
+++ server/src/video_processing/main.py
@@ -1,2 +1,6 @@
 # this file/folder will contain code with openCV to monitor user interactions and if they are paying attention
 # openCV does have a js port, which we could consider
+
+import cv2
+
+print(cv2.__version__)
